ac649a94e8570ffcb0424147f86aa558af5d380d
BenFielder1 <15e16347@marling.school>
2021-05-14 11:28:38 +0100

    testing transfer of ownership

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom'
 import Game1 from './game1'
 import Game2 from './game2'
 
+import Game from "./game5"
+
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game1 />
-    <Game2 />
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 )
