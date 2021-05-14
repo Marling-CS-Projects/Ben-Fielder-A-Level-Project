a3e43550bf68d6119b612a7a595fbd962ab2d414
BenFielder1 <15e16347@marling.school>
2021-05-14 11:31:56 +0100

    code will now run

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import Game1 from './game1'
 import Game2 from './game2'
 
-import Game from "./game5"
+//import Game from "./game5"
 
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
+    <Game1 />
+    <Game2 />
   </React.StrictMode>,
   document.getElementById('root')
 )
